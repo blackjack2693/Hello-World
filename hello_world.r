61b9558270a8887ac815ebc13844650663c5fa30
Ida911 <ida.schulze.tast@uni-muenster.de>
2024-09-24 12:50:09 +0200

    Rechtschreibung
    Fehler behoben

--- hello_world.r
+++ hello_world.r
@@ -4,5 +4,5 @@ for (i in 1:10) {
 
 install.packages("cowsay")
 library(cowsay)
-say(what="Halli hallo ida, wie ghet es dir?", by="grumpycat")
+say(what="Halli hallo ida, wie geht es dir?", by="grumpycat")
 say(what="gut und dir?", by="ghost")
