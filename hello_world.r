4f8a1c3eb275a0d98ced5b4fcad95343245ed8f0
Johann Schulze Tast <johann.schulzetast@student.hpi.de>
2024-09-24 12:46:05 +0200

    changes to the cowsay

--- hello_world.r
+++ hello_world.r
@@ -1,7 +1,7 @@
-for (i in 1:100) {
+for (i in 1:10) {
 	print("Hello, World!")
 }
 
 install.packages("cowsay")
 library(cowsay)
-say(what="hallo", by="shark")
+say(what="Halli hallo ida, wie ghet es dir?", by="grumpycat")
