b01d385c451f7a40e5ff0be4782c7c76868c389e
Johann Schulze Tast <johann.schulzetast@student.hpi.de>
2024-09-24 12:50:46 +0200

    conflict introduced

--- hello_world.r
+++ hello_world.r
@@ -4,4 +4,6 @@ for (i in 1:10) {
 
 install.packages("cowsay")
 library(cowsay)
-say(what="Halli hallo ida, wie ghet es dir?", by="grumpycat")
+say(what="Halli hallo ida, wie geht es uns heute?", by="grumpycat")
+
+print("jetzt basteln wir Konflike!")
